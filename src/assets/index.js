225bb1568613e3b1730cc118eb3494858efe4063
developerjay18 <jayrai3003@gmail.com>
2023-10-21 22:02:15 +0530

    adding images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,19 @@
 import BG from './bg.webp';
 import HeroIcon from './hero-icon.svg';
 import WhatsappIcon from './whatsapp.png';
+import FooterBG from './footer-bg.png';
+import LinkedinIcon from './linkedin.png';
+import GithubIcon from './github.png';
+import GmailIcon from './gmail.png';
+import InstaIcon from './instagram.png';
 
-export { BG, HeroIcon, WhatsappIcon };
+export {
+  BG,
+  HeroIcon,
+  WhatsappIcon,
+  FooterBG,
+  LinkedinIcon,
+  GithubIcon,
+  GmailIcon,
+  InstaIcon,
+};
