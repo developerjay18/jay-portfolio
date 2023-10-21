200b1763ba1234bd814c48a72b3299e794d710cd
developerjay18 <jayrai3003@gmail.com>
2023-10-21 18:18:53 +0530

    creating pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeroSection, Container } from '../components';
+
+function Home() {
+  return (
+    <Container>
+      <HeroSection />
+    </Container>
+  );
+}
+
+export default Home;
