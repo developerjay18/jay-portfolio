6c03cfbfb1d1860eae60bc9a39495d7dd0592211
developerjay18 <jayrai3003@gmail.com>
2023-10-21 12:32:05 +0530

    adding custon styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      whitePrimary: '#F0F2F5',
+      whiteSecondary: '#ffffff',
+      blackPrimary: '#161513',
+      blackSecondary: '#1C1C22',
+      gradientPurple: '#B16CEA',
+      gradientCarrot: '#FF5E69',
+      gradientOrange: '#FF8A56',
+      gradientYellow: '#FFA84B',
+      lightGrey: '#c5c3c3',
+    },
+    fontFamily: {
+      baiJamjuree: ['Bai Jamjuree', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
