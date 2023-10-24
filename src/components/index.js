16e48433e95a06c762489da8fad2bea085b9200e
developerjay18 <jayrai3003@gmail.com>
2023-10-24 13:10:17 +0530

    adding fontawesome link

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,9 @@ import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Container from './Container/Container.jsx';
 import HeroSection from './HeroSection/HeroSection';
+import AboutSection from './AboutSection/AboutSection';
+import Stats from './AboutSection/Stats';
+import SkillsSection from './SkillsSection/SkillsSection';
+import TechTools from './SkillsSection/Techtools';
 
-export { Footer, Header, Container, HeroSection };
+export { Footer, Header, Container, HeroSection, AboutSection, Stats,SkillsSection ,TechTools};
