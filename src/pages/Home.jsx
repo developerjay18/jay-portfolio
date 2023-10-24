ce07af4ff9efc794e3897a434b0ff86c280e6909
developerjay18 <jayrai3003@gmail.com>
2023-10-24 13:11:02 +0530

    creating home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { HeroSection, Container } from '../components';
+import { HeroSection, Container, AboutSection,SkillsSection } from '../components';
 
 function Home() {
   return (
     <Container>
       <HeroSection />
+      <AboutSection />
+      <SkillsSection/>
     </Container>
   );
 }
