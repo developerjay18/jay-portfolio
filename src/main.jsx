49ac0b8d12f742ffd6b78b00ae1a8b03d1ddf866
developerjay18 <jayrai3003@gmail.com>
2023-10-26 11:38:34 +0530

    modifying main file

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,16 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import { Home, AboutMe, Contact, Projects, Skills } from './pages/index.js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<Home />} />
+      <Route path="/about-me" element={<AboutMe />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/skills" element={<Skills />} />
     </Route>
   )
 );
