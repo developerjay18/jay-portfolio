3aa83eb2daab1f6f7f3f707605f193f555b46610
developerjay18 <jayrai3003@gmail.com>
2023-10-26 16:41:22 +0530

    connecting from with emailjs

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Developer } from '../../assets';
+import emailjs from '@emailjs/browser';
+import config from '../../config/config';
 
 function ContactSection() {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value }); // 2nd approch also available
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        config.emailjsServiceID,
+        config.emailjsTemplateID,
+        data,
+        config.emailjsPublicID
+      )
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+      })
+      .catch((error) => {
+        console.error('Email could not be sent:', error);
+      });
+  };
+
   return (
     <div className="font-baiJamjuree px-7 md:px-24 py-8 lg:py-14 bg-[#121212] flex justify-center items-center md:items-start text-whitePrimary gap-10 md:gap-8 flex-col md:flex-row">
       <div className="left-up w-full md:w-1/2 flex justify-center md:justify-start">
@@ -22,17 +53,17 @@ function ContactSection() {
             action="#"
             method="post"
             className="flex flex-col gap-5"
-            onClick={(e) => {
-              e.preventDefault();
-            }}
+            onSubmit={handleSubmit}
           >
             <div className="flex gap-5 flex-col md:flex-row">
               <input
                 type="text"
-                name="f-name"
-                id="f-name"
+                name="name"
+                id="name"
                 placeholder="first name"
                 className="capitalize p-2 py-3 glassy-div md:w-1/2 rounded-md focus:outline-none cursor-pointer focus:bg-[#474747] w-full"
+                value={data.name}
+                onChange={handleChange}
               />
               <input
                 type="email"
@@ -40,15 +71,19 @@ function ContactSection() {
                 id="email"
                 placeholder="email address"
                 className="w-full md:w-1/2 p-2 py-3 glassy-div rounded-md focus:outline-none cursor-pointer focus:bg-[#474747]"
+                value={data.email}
+                onChange={handleChange}
               />
             </div>
             <textarea
-              name="msg"
-              id="msg"
+              name="message"
+              id="message"
               cols="30"
               rows="6"
               className="w-full glassy-div rounded-md p-2 focus:outline-none cursor-pointer focus:bg-[#474747]"
               defaultValue="Message"
+              value={data.message}
+              onChange={handleChange}
             ></textarea>
             <button
               type="submit"
