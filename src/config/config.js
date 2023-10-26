803016a487bdac53c0219c54293f3c6f43b0d06a
developerjay18 <jayrai3003@gmail.com>
2023-10-26 16:41:52 +0530

    adding enviornment variables

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,9 @@ const config = {
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  emailjsServiceID: String(import.meta.env.VITE_EMAILJS_SERVICEID),
+  emailjsTemplateID: String(import.meta.env.VITE_EMAILJS_TEMPLATEID),
+  emailjsPublicID: String(import.meta.env.VITE_EMAILJS_PUBLICKEY),
 };
 
 export default config;
