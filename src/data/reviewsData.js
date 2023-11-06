f7a7bfdbf70dd48a499669eeda0280e0d37a1890
developerjay18 <jayrai3003@gmail.com>
2023-11-06 08:26:26 +0530

    adding data into arrays

---
+++ src/data/reviewsData.js
@@ -0,0 +1,52 @@
+import * as ProfileImg from '../assets/reviews';
+
+const data = [
+  {
+    imageUrl: ProfileImg.Ravi,
+    name: 'ravi rajbhar',
+    profession: 'Founder of clothing brand',
+    location: 'banglore',
+    review:
+      'Your website development skills are truly remarkable. The animated features on our site have captivated our visitors and boosted our engagement significantly.',
+  },
+  {
+    imageUrl: ProfileImg.Yash,
+    name: 'yash sharma',
+    profession: 'digital marketer',
+    location: 'ahmedabad',
+    review:
+      'Our business witnessed an incredible transformation after you developed our website. Your animated designs played a significant role in boosting our revenue by 5x!',
+  },
+  {
+    imageUrl: ProfileImg.Mohini,
+    name: 'mohini sharma',
+    profession: 'google ads expert',
+    location: 'rajasthan',
+    review: `You've worked wonders for our business with your website development services. Our site is not just functional; it's a visual masterpiece thanks to your animated designs`,
+  },
+  {
+    imageUrl: ProfileImg.Avinash,
+    name: 'avinash pandey',
+    profession: 'accounting consultant',
+    location: 'punjab',
+    review:
+      'Your full stack development services are top-notch. The website you created for us is a work of art, and our business has flourished thanks to your expertise.',
+  },
+  {
+    imageUrl: ProfileImg.Madin,
+    name: 'madin bloch',
+    profession: 'web developer',
+    location: 'junagadh',
+    review:
+      'Working with you has been a pleasure, and the results speak for themselves. Your website development expertise has given our business an incredible boost',
+  },
+  {
+    imageUrl: ProfileImg.Astrologer,
+    name: 'yogesh sharma',
+    profession: 'Astrologer',
+    location: 'mumbai',
+    review: `Your website development services have been a game-changer for our online presence. We've witnessed a significant increase in leads and conversions since we launched our new site`,
+  },
+];
+
+export default data;
