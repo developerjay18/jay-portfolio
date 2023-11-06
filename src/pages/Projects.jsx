9d7fe78b49fb15e836edbd804ffc92b2e615e9a2
developerjay18 <jayrai3003@gmail.com>
2023-11-06 08:31:03 +0530

    adding reviews to projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Container, ProjectsSection } from '../components';
+import { Container, ProjectsSection,SliderSection } from '../components';
 
 function Projects() {
   return (
     <Container>
       <ProjectsSection />
+      <SliderSection/>
     </Container>
   );
 }
