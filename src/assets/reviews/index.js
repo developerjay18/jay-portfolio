740fce62bd7eb81410833ef4440bfae69e0fa9dc
developerjay18 <jayrai3003@gmail.com>
2023-11-06 08:25:04 +0530

    adding clients reviews

---
+++ src/assets/reviews/index.js
@@ -0,0 +1,8 @@
+import Yash from './yash.jfif';
+import Ravi from './ravi.jfif';
+import Mohini from './mohini.jfif';
+import Avinash from './pandey.jfif';
+import Madin from "./madin.jfif"
+import Astrologer from "./astrologer.png"
+
+export {Yash,Mohini,Ravi,Avinash,Madin,Astrologer}
